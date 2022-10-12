348e69850b90543df5d9f75254766a756b8cb2d4
MatCast <gozyssj4@gmail.com>
2022-10-12 16:33:19 +0200

    Add foramt data.

--- temperature_bmp.py
+++ temperature_bmp.py
@@ -12,8 +12,18 @@ bme280.load_calibration_params(bus, address)
 def read_all():
     bme280_data = bme280.sample(bus, address)
     data = {
-        'temperature': bme280_data.temperature,
-        'humidity': bme280_data.humidity,
-        'pressure': bme280_data.pressure
+        "temperature": bme280_data.temperature,
+        "humidity": bme280_data.humidity,
+        "pressure": bme280_data.pressure,
     }
+    return data
+
+
+def read_all_and_format():
+    data = read_all()
+    data_fmt = (
+        f"Temperature: {data['temperature']:.2f} °C"
+        f"\nHumidity: {data['humidity']:.2f} %"
+        f"\nPressure: {data['pressure']:.2f} bar\n"
+    )
     return data
